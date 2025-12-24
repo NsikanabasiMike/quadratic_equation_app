95226535b09ae2f1c789728901d00d2c9cb1ba46
Nsikanabasi <essietnsikan4@gmail.com>
2025-12-24 10:52:55 +0100

    Revert to previous Version

--- quadratic_equation_app.py
+++ quadratic_equation_app.py
@@ -2,19 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
-import os
 
-st.set_page_config(page_title='Quadratic Equation Solver With Graph', layout="wide")
+st.set_page_config(page_title='Essiet Quadratic App')
+st.title('Quadratic Equation App')
 
 
-st.markdown(
-    "<h1 style='text-align: center;'>Quadratic Equation Solver</h1>",
-    unsafe_allow_html=True
-)
-# st.title('Quadratic Equation Solver')
-
-# Quadratic equation solution function
-def quadratic_solution(a, b, c):
+# Quadratic equation solution fuction
+def quadratic_solution(a, b, c, lower_x, upper_x):
     '''
     Provide roots to a quadratic equation based on given inputs.
     
@@ -25,44 +19,39 @@ def quadratic_solution(a, b, c):
     Returns:
         result
         plot (fig)
+        dataframe (df)
 
     '''
-
-    lower_x = -20
-    upper_x = 20
     if a == 0:
-        return f'Variable _a_ cannot be zero', None
+        return f'Variable a cannot be zero'
 
+        
+    x_values = np.linspace(lower_x, upper_x, 40)
+    y_values = a * x_values * x_values + b * x_values + c
+
+    # Dataframe
+    df = pd.DataFrame({'x': x_values, 'y': y_values})
+
+    df = df.T   # ---Dataframe transposed
     fig, ax = plt.subplots(figsize = (5, 3))
+    ax.plot(x_values, y_values)
+    ax.set_xlabel("x values")
+    ax.set_ylabel("y values")
 
     # Discriminant
     d = (b)**2 - (4 * a * c)
-
-    if d < 0: 
-        x_values = np.linspace(lower_x, upper_x, 400)
-        y_values = a * x_values * x_values + b * x_values + c
-        ax.plot(x_values, y_values)
-
-        ax.set_xlabel("x values")
-        ax.set_ylabel("y values")
+    if d < 0:
         ax.set_title('No Real Solution')
-        return f'Sorry, no solution', fig
+        return f'Sorry, no solution', fig, df
     
     if d==0:
         single_root = round((-b) / (2 * a), 2)
-
-        lower_x = int(single_root + lower_x)
-        upper_x = int(single_root + upper_x)
-
-        x_values = np.linspace(lower_x, upper_x, 400)
-        y_values = a * x_values * x_values + b * x_values + c
-        ax.plot(x_values, y_values)
+        ax.plot(single_root,  0, marker = 'o', color = 'r')
 
         # Plot for a single root
-        ax.plot(single_root,  0, marker = 'o', color = 'r')
         ax.text(single_root, 0, f"({single_root:.2f})", color='red', fontsize=8, ha='right', va='top')
         ax.set_title('Equation with one solution')
-        return f'Solution to the equation: {single_root}', fig
+        return f'Solution to the equation: {single_root}', fig, df
 
     d1 = d**(1/2)
     root_1 = round((-b + d1)/ (2 * a), 2)
@@ -70,24 +59,17 @@ def quadratic_solution(a, b, c):
     roots = sorted([root_1, root_2])
     alignment = ['right', 'left' ]
 
-    lower_x = int(roots[0] + lower_x)
-    upper_x = int(roots[1] + upper_x)
-    x_values = np.linspace(lower_x, upper_x, 400)
-    y_values = a * x_values * x_values + b * x_values + c
-    ax.plot(x_values, y_values)
-
     # Plot for two roots
     for idx in range(len(roots)):
         ax.plot(roots[idx],  0, marker = 'o', color = 'r')
         ax.text(roots[idx], 0, f"({roots[idx]:.2f})", color='red', fontsize=8, ha=alignment[idx], va='top')
 
     ax.set_title('Equation with two solutions')
-    return f'Solutions to the equation are: {roots[0]} and {roots[1]}', fig
+    return f'Solutions to the equation are: {roots[0]} and {roots[1]}', fig, df
 
-# Columns
-col1, col2, col3 = st.columns([1.5, 0.20, 1])
+st.sidebar.header('User Data')
 
-# User data function
+# Sidebar block function
 def user_data():
     '''
         Supply user inputs for a quadratic equation.
@@ -98,53 +80,56 @@ def user_data():
             a: coefficient of 2nd powered term
             b: coefficient of 1st powered term
             c: constant
+            lower_x: min range of x
+            upper_x: max range of x
+
     '''
+    a = st.sidebar.slider('Constant of 2nd powered term ($ax^2$)  : ', -100, 100, 1)
+    b = st.sidebar.slider('Constant of 1st powered term (bx): ', -100, 100, 1)
+    c = st.sidebar.slider('Constant (c) or bias: ', -100, 100, 1)
+    lower_x = st.sidebar.slider('Min. range of x:', -300, 0, -10, 1)
+    upper_x = st.sidebar.slider('Max. range of x:', 0, 300, 10, 1)
 
-    with col1:
-        col1.subheader('Input Data')
-        with st.form(key = 'form info', enter_to_submit = False, clear_on_submit = False):
-            
-            a = st.number_input('Constant of 2nd powered term ($ax^2$)  : ', value = 1, step = 1)
-            b = st.number_input('Constant of 1st powered term (bx): ', value = 0, step = 1)
-            c = st.number_input('Constant (c) or bias: ', value = 0, step = 1)
-            submit = st.form_submit_button()
-
-
-    return a, b, c
+    return a, b, c, lower_x, upper_x
 
 # user_data execution block
-a, b, c = user_data()
+a, b, c, lower_x, upper_x = user_data()
+
+
+# Columns
+col1, col2, col3 = st.columns([1.5, 0.20, 1])
 
 # Write in column 3
 with col3:
 
     with st.expander('Quadratic Equation App Details and Usage'):
-        st.image(os.path.join(os.getcwd(), 'static', 'IMG-20251222-WA0004.jpg'), width = 200)
         st.write('''
-            📈 Solve and visualize quadratic equations with ease! 📊
 
-            Quadratic Equations Web App lets you input the coefficients (a, b, c) and:
-            - View the graph of the equation
-            - Get the roots (real and complex)
-            - Understand quadratic equation concepts better
-            - Perfect for students, teachers, and math enthusiasts!
+            This App displays the graphical solution to quadratic equations based on the user's inputs.
+            It works on the function: $y = ax^2 + bx + c$.
+            Variables a, b, and c are constants. a = constant of second powered term $x^2$,
+            b = constant of first powered term $bx$
+            A quadratic equation has a solution when y = 0.
 
-            *Why use it?*:
-            - Instant graph visualization 📊
-            - Step-by-step understanding of solutions
-            - Handy tool for math practice & exams
+            This app is dynamic. To use it, simply adjust the parameters on the side bar :blue[👈].
+            Parameters like **_Min. range of x_** and **_Max. range of x_** control the range of x and can be adjusted to fit the line to the roots if not fitted properly.
 
-            FREE to download, with occasional ads to support the app.
+            Enjoy the app :blue[👍]
 
             ''')
 
 # quadratic_solution execution block
-col3.subheader('Result')
-result, plot = quadratic_solution(a, b, c)
-
-with col3:
+col1.subheader('Result')
+try:
+    result, plot, df = quadratic_solution(a, b, c, lower_x, upper_x)
     if plot is not None:
-        st.pyplot(plot)
-        col3.markdown(f'**{result}**')
-    else:
-        col3.markdown(f'**{result}**')
+        with col1:
+            st.pyplot(plot)
+            st.markdown(f'**{result}**')
+
+        with col3:
+            st.divider()
+            st.markdown('**Table of values**')
+            st.dataframe(df)
+except ValueError:
+    col1.write('_Constant of second powered term cannot be zero_')
